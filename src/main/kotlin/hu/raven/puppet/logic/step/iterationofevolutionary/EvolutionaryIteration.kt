067d90e20fffbbcd6460450dc722fa76379cb690
Hollo1996 <holloszaboakos.1996@gmail.com>
2023-04-16 12:36:43 +0200

    refactored iteration

--- src/main/kotlin/hu/raven/puppet/logic/step/iterationofevolutionary/EvolutionaryIteration.kt
+++
@@ -1,9 +0,0 @@
-package hu.raven.puppet.logic.step.iterationofevolutionary
-
-import hu.raven.puppet.logic.logging.DoubleLogger
-import hu.raven.puppet.model.physics.PhysicsUnit
-
-sealed class EvolutionaryIteration<C : PhysicsUnit<C>> {
-    abstract val logger: DoubleLogger
-    abstract operator fun invoke()
-}
